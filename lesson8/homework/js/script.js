55a4cccb53d37a69d38f313d1bca0bbed1cbbe5a
VitaliiKosyk <kostamtik@gmail.com>
2021-11-30 14:13:45 +0200

    lesson 8 edit /homework/, /classwork/

--- lesson8/homework/js/script.js
+++ lesson8/homework/js/script.js
@@ -4,22 +4,22 @@
 
 
 //     + отримує текст з параграфа з id "content"
-let idContent = document.getElementById('content')
-console.log(idContent.innerText)
+let idContent = document.getElementById('content');
+console.log(idContent.innerText);
 
 //     + отримує текст з блоку з id "rules"
-let idRules = document.getElementById('rules')
-console.log(idRules.innerText)
+let idRules = document.getElementById('rules');
+console.log(idRules.innerText);
 
 //     + замініть текст параграфа з id 'content' на будь-який інший
 idContent.innerText = 'innerText - это свойство, позволяющее задавать или получать текстовое содержимое элемента и его потомков.' +
     ' В качестве геттера, свойство приближается к тексту, который пользователь получит, если он выделит содержимое ' +
-    'элемента курсором, затем копирует его в буфер обмена.'
-console.log(idContent.innerText)
+    'элемента курсором, затем копирует его в буфер обмена.';
+console.log(idContent.innerText);
 
 //     + замініть текст параграфа з id 'rules' на будь-який інший
-idRules.innerText = 'innerText умеет считывать стили и не возвращает содержимое скрытых элементов, тогда как textContent этого не делает.'
-console.log(idRules.innerText)
+idRules.innerText = 'innerText умеет считывать стили и не возвращает содержимое скрытых элементов, тогда как textContent этого не делает.';
+console.log(idRules.innerText);
 
 //     + змініть кожному елементу колір фону на червоний
 function changeColor(startElement) {
@@ -27,9 +27,9 @@ function changeColor(startElement) {
         for (const element of startElement.children) {
             changeColor(element);
         }
-    }else startElement.style.background = '#ff6392'
+    }else startElement.style.background = '#ff6392';
 }
-changeColor(document.body)
+changeColor(document.body);
 
 //     + змініть кожному елементу колір тексту на синій
 function changeColorText(startElement) {
@@ -37,16 +37,16 @@ function changeColorText(startElement) {
         for (const element of startElement.children) {
             changeColorText(element);
         }
-    }else startElement.style.color = '#60c1e7'
+    }else startElement.style.color = '#60c1e7';
 }
-changeColorText(document.body)
+changeColorText(document.body);
 
 //     + отримати весь список класів елемента з id=rules і вивести їх в console.log
-let classArr = document.getElementById('rules')
-console.log(classArr.className)
+let classArr = document.getElementById('rules');
+console.log(classArr.className);
 
 //     + поміняти колір тексту у всіх елементів fc_rules на червоний
-let className = document.getElementsByClassName('fc_rules')
+let className = document.getElementsByClassName('fc_rules');
 for (const classNameElement of className) {
-    classNameElement.style.color = '#c91212'
+    classNameElement.style.color = '#c91212';
 }
